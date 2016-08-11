e73afed09673c5c1896a2216db5d4b6ac812b8d7
jack <yangj26952@gmail.com>
2016-08-11 22:37:10 +0800

    Finish example3

--- example3.jsx
+++ example3.jsx
@@ -2,10 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class Example3 extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			englishName: '',
+			showName:''
+     		};
+
+     		this.handlerInputName = this.handlerInputName.bind(this);
+     		this.handlerSubmit = this.handlerSubmit.bind(this);
+	}
+   
+
+	handlerInputName(e){
+		this.setState({englishName: e.target.value});
+	}
+
+	handlerSubmit(e) {
+		
+		 this.setState({showName: this.state.englishName});
+		// console.log();
+	}
+
 
 	render(){
+		const result = this.inputName;
 		return (
-			<div>Hello Example3 CCCCC</div>
+			<div>
+				<p>
+					Input Your Name:
+					<input type='text' name='yourname' onChange={this.handlerInputName} />
+				</p>
+				<p>
+					<button onClick={this.handlerSubmit}>Submit</button>
+				</p>
+				<p>
+					{this.state.showName}
+				</p>
+			</div>
 		);
 	}
 }
