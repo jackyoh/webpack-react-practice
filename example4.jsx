6b05c29eceb39182778421ab73bce813dd89d486
jackyoh <yangj26952@gmail.com>
2016-08-12 00:35:40 +0800

    practice reactjs

--- example4.jsx
+++ example4.jsx
@@ -3,9 +3,41 @@ import ReactDOM from 'react-dom';
 
 export default class Example4 extends React.Component {
 
+	constructor(props){
+		super(props);
+		this.state = {
+			inputValue: '',
+			showLi: ['bbb','cccc'],
+		};
+		this.handlerInputName = this.handlerInputName.bind(this);
+		this.handlerSubmit = this.handlerSubmit.bind(this);
+	}
+
+	handlerInputName(e){
+		this.setState({inputValue: e.target.value});
+	}
+
+	handlerSubmit(e){
+		this.setState({showLi: this.state.showLi.concat(this.state.inputValue)});
+		console.log(this.state.showLi.length);
+	}
+
 	render(){
 		return (
-			<div>Hello Example4 DDDDDD</div>
+			<div>
+				<p>
+					Input Your Name:
+					<input type='text' name='yourname' onChange={this.handlerInputName} />
+				</p>
+				<p>
+					<button onClick={this.handlerSubmit}>Submit</button>
+				</p>
+				<p>
+					<ul>{this.state.showLi.map(function(key){
+						return <li>{key}</li> ;
+					})}</ul>
+				</p>
+			</div>
 		);
 	}
 }
