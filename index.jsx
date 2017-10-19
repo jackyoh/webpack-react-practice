1616eec2bbcb384ffbb5b4f1b922789fe771fe70
Konekoya <tank158r5155@yahoo.com.tw>
2017-10-19 15:51:50 +0800

    Refactor

--- index.jsx
+++ index.jsx
@@ -1,13 +1,9 @@
-require("./node_modules/bootstrap/dist/css/bootstrap.min.css")
+require('./node_modules/bootstrap/dist/css/bootstrap.min.css');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export class App extends React.Component {
-	render() {
-		return (
-			<div>WebPack Example</div>
-		);
-	}
-}
+const App = () => {
+  return <div>WebPack Example</div>;
+};
 
-ReactDOM.render(<App/>, document.querySelector("#myApp"));
+ReactDOM.render(<App />, document.querySelector('#myApp'));
--- tool.jsx
+++ tool.jsx
@@ -1,42 +1,41 @@
-require("./node_modules/bootstrap/dist/css/bootstrap.min.css")
+require('./node_modules/bootstrap/dist/css/bootstrap.min.css');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 export class Tool extends React.Component {
-	
-     handleExample1 () {
-     		console.log('click example1');
-     }
+  handleExample1() {
+    console.log('click example1');
+  }
 
-     handleExample2() {
-           console.log('click example2');
-     }
+  handleExample2() {
+    console.log('click example2');
+  }
 
-     handleExample3() {
-           console.log('click example3');
-     }
+  handleExample3() {
+    console.log('click example3');
+  }
 
-     handleExample4() {
-     	      console.log('click example4');
-     }
+  handleExample4() {
+    console.log('click example4');
+  }
 
-     handleExample5() {
-            console.log('click example5');
-     }
+  handleExample5() {
+    console.log('click example5');
+  }
 
-	render() {
-		return (
-			<div>
-                    <ul>
-			       <li onClick = { this.handleExample1 }>example1</li>
-			       <li onClick = { this.handleExample2 }>example2</li>
-			       <li onClick = { this.handleExample3 }>example3</li>
-			       <li onClick = { this.handleExample4 }>example4</li>
-			       <li onClick = { this.handleExample5 }>example5</li>
-                    </ul>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <ul>
+          <li onClick={this.handleExample1}>example1</li>
+          <li onClick={this.handleExample2}>example2</li>
+          <li onClick={this.handleExample3}>example3</li>
+          <li onClick={this.handleExample4}>example4</li>
+          <li onClick={this.handleExample5}>example5</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<Tool/>, document.querySelector("#tool"));
+ReactDOM.render(<Tool />, document.querySelector('#tool'));
